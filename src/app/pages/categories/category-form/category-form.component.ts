44412d8f15aa1a542cfedd4833fd58cb32e1d356
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2020-06-07 01:41:14 -0300

    finally create and update category

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -40,11 +40,23 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle();
   }
 
-  private setCurrentAction(): void {
-    this.activatedRoute.snapshot.url[0].path == 'new'
-      ? (this.currentAction = 'new')
-      : 'edit';
+  submitForm(): void {
+    this.submittingForm = true;
+    if (this.currentAction == 'new') {
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
   }
+
+  private setCurrentAction(): void {
+    if (this.activatedRoute.snapshot.url[0].path == 'new') {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit';
+    }
+  }
+
   private buildCategoryForm(): void {
     this.categotyForm = this.formBuilder.group({
       id: [null],
@@ -53,7 +65,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
         [
           Validators.required,
           Validators.minLength(2),
-          Validators.pattern(/\d.*/),
+          Validators.maxLength(50),
         ],
       ],
       description: [null, { disabled: true }],
@@ -61,18 +73,19 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   }
 
   private loadCategory(): void {
-    if (this.currentAction != 'edit') return;
-    this.activatedRoute.paramMap
-      .pipe(
-        switchMap((param) => this.categoryService.getById(+param.get('id')))
-      )
-      .subscribe(
-        (category) => {
-          this.category = category;
-          this.categotyForm.patchValue(this.category);
-        },
-        (error) => console.log('Ocorreu um erro no servidor', error)
-      );
+    if (this.currentAction == 'edit') {
+      this.activatedRoute.paramMap
+        .pipe(
+          switchMap((param) => this.categoryService.getById(+param.get('id')))
+        )
+        .subscribe(
+          (category) => {
+            this.category = category;
+            this.categotyForm.patchValue(this.category);
+          },
+          (error) => console.log('Ocorreu um erro no servidor', error)
+        );
+    }
   }
 
   private setPageTitle(): void {
@@ -82,5 +95,49 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       const categoryName = this.category.name || '';
       this.pageTitle = 'Editando Categoria: ' + categoryName;
     }
+  }
+
+  private createCategory(): void {
+    const category: Category = {
+      ...new Category(),
+      ...this.categotyForm.value,
+    };
+
+    this.categoryService.create(category).subscribe(
+      (category) => this.actionFormSuccess(category),
+      (error) => this.actionFormError(error)
+    );
+  }
+
+  private actionFormSuccess(category: Category): void {
+    toastr.success('Solicitação processada com sucesso');
+
+    this.router
+      .navigateByUrl('categories', { skipLocationChange: true })
+      .then(() => this.router.navigate(['categories', category.id, 'edit']));
+  }
+
+  private actionFormError(error: any): void {
+    toastr.error('Ocorreu um erro ao processar a sua solicitação');
+    this.submittingForm = false;
+    if (error.status == 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = [
+        'Falha na comunicação com o servidor. Por favor, tente mais tarde',
+      ];
+    }
+  }
+
+  private updateCategory(): void {
+    const category: Category = {
+      ...new Category(),
+      ...this.categotyForm.value,
+    };
+
+    this.categoryService.update(category).subscribe(
+      (category) => this.actionFormSuccess(category),
+      (error) => this.actionFormError(error)
+    );
   }
 }
