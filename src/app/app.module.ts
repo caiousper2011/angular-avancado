fe585c8389d992eb780d2ea140d46202599dbdbe
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2020-06-28 01:37:36 -0300

    project structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
+  imports: [AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
