003df03806f7116d274ed209f49eb2861def231e
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2020-06-28 00:47:08 -0300

    adding category to entry

--- src/app/pages/entries/shared/services/entry.service.ts
+++ src/app/pages/entries/shared/services/entry.service.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from './../../../categories/shared/category.service';
 import { environment } from '../../../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -10,7 +11,10 @@ import { Entry } from '../models/entry.model';
 export class EntryService {
   private apiPath: string = `${environment.apiUrl}/entries`;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService
+  ) {}
 
   getAll(): Observable<Entry[]> {
     return this.http
@@ -25,17 +29,30 @@ export class EntryService {
       .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
   }
 
-  create(category: Entry): Observable<Entry> {
-    return this.http
-      .post<Entry>(this.apiPath, category)
-      .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+  create(entry: Entry): Observable<Entry> {
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+
+        return this.http
+          .post<Entry>(this.apiPath, entry)
+          .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+      })
+    );
   }
 
-  update(category: Entry): Observable<Entry> {
-    const url = `${this.apiPath}/${category.id}`;
-    return this.http.put(url, category).pipe(
-      catchError(this.handleError),
-      map(() => category)
+  update(entry: Entry): Observable<Entry> {
+    const url = `${this.apiPath}/${entry.id}`;
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+      })
     );
   }
 
