5431b812ed401fa060337aad22769703607f76fb
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2020-06-29 00:06:59 -0300

    improving entryService

--- src/app/pages/entries/shared/services/entry.service.ts
+++ src/app/pages/entries/shared/services/entry.service.ts
@@ -2,8 +2,9 @@ import { CategoryService } from './../../../categories/shared/category.service';
 import { Injectable, Injector } from '@angular/core';
 import { Entry } from '../models/entry.model';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { runInThisContext } from 'vm';
 @Injectable({
   providedIn: 'root',
 })
@@ -16,20 +17,23 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap((category) => {
-        entry.category = category;
-        return super.create(entry);
-      })
-    );
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  private setCategoryAndSendToServer(
+    entry: Entry,
+    sendFn: any
+  ): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap((category) => {
         entry.category = category;
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     );
   }
 }
