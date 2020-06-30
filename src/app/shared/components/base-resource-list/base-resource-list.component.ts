cb668cfab81e1bd9624c5af71e5e26efdf3a9c3b
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2020-06-30 02:48:03 -0300

    creating baselist

---
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -0,0 +1,27 @@
+import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+
+import { OnInit } from '@angular/core';
+import { BaseResourceModel } from '../../models/base-resource.model';
+
+export abstract class BaseResourceList<T extends BaseResourceModel>
+  implements OnInit {
+  resources: T[] = [];
+  constructor(private resourceService: BaseResourceService<T>) {}
+
+  ngOnInit(): void {
+    this.resourceService.getAll().subscribe(
+      (resources) => (this.resources = resources),
+      (error) => alert('Erro ao carregar Lista')
+    );
+  }
+
+  deleteCategory(resource: T): void {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+    if (!mustDelete) return;
+    this.resourceService.delete(resource.id).subscribe(
+      () =>
+        (this.resources = this.resources.filter((item) => item != resource)),
+      (error) => console.log('Erro ao tentar excluir', error)
+    );
+  }
+}
